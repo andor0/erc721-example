b6efd0ceb736e8d4e57d15614105f184cfe89295
Andrei Orlov <aaorlov1@gmail.com>
2023-02-05 18:25:46 +0400

    Formatting

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,70 +1,76 @@
-use axum::{
-    extract::State,
-    routing::get,
-    response::IntoResponse,
-    Json, Router,
-};
+use axum::{extract::State, response::IntoResponse, routing::get, Json, Router};
 use ethabi::ParamType;
 use hex_literal::hex;
 use serde::Serialize;
 use serde_json::json;
-use std::{net::SocketAddr, sync::{Arc, RwLock}};
-use web3::{error::Error as Web3Error, futures::{future, StreamExt}, types::{FilterBuilder, H256}};
+use std::{
+    net::SocketAddr,
+    sync::{Arc, RwLock},
+};
+use web3::{
+    error::Error as Web3Error,
+    futures::{future, StreamExt},
+    types::{FilterBuilder, H256},
+};
 
 // web3.utils.keccak256('CollectionCreated(address,string,string)')
-const TOPIC_COLLECTION_CREATED: H256 = H256(hex!("3454b57f2dca4f5a54e8358d096ac9d1a0d2dab98991ddb89ff9ea1746260617"));
+const TOPIC_COLLECTION_CREATED: H256 = H256(hex!(
+    "3454b57f2dca4f5a54e8358d096ac9d1a0d2dab98991ddb89ff9ea1746260617"
+));
 // web3.utils.keccak256('TokenMinted(address,address,uint256,string)')
-const TOPIC_TOKEN_MINTED: H256 = H256(hex!("c9fee7cd4889f66f10ff8117316524260a5242e88e25e0656dfb3f4196a21917"));
+const TOPIC_TOKEN_MINTED: H256 = H256(hex!(
+    "c9fee7cd4889f66f10ff8117316524260a5242e88e25e0656dfb3f4196a21917"
+));
 
 type Events = Vec<Event>;
 
 #[derive(Serialize)]
 enum Event {
-    CollectionCreated { collection: String, name: String, symbol: String },
-    TokenMinted {collection: String, recipient: String, token_id: String, token_uri: String },
+    CollectionCreated {
+        collection: String,
+        name: String,
+        symbol: String,
+    },
+    TokenMinted {
+        collection: String,
+        recipient: String,
+        token_id: String,
+        token_uri: String,
+    },
 }
 
 impl Event {
     fn from_log(log: web3::types::Log) -> Result<Event, Web3Error> {
-        match (&log.topics.contains(&TOPIC_COLLECTION_CREATED), &log.topics.contains(&TOPIC_TOKEN_MINTED)) {
-            (true, false) => {
-                ethabi::decode(
-                    &[
-                        ParamType::Address,
-                        ParamType::String,
-                        ParamType::String,
-                    ],
-                    &log.data.0,
-                )
-                .map(|params| {
-                    Event::CollectionCreated {
-                        collection: params[0].to_string(),
-                        name: params[1].to_string(),
-                        symbol: params[2].to_string()
-                    }
-                })
-                .map_err(|_| Web3Error::Internal)
-            },
-            (false, true) => {
-                ethabi::decode(
-                    &[
-                        ParamType::Address,
-                        ParamType::Address,
-                        ParamType::Uint(256),
-                        ParamType::String,
-                    ],
-                    &log.data.0,
-                )
-                .map(|params| {
-                    Event::TokenMinted {
-                        collection: params[0].to_string(),
-                        recipient: params[1].to_string(),
-                        token_id: params[2].to_string(),
-                        token_uri: params[3].to_string(),
-                    }
-                })
-                .map_err(|_| Web3Error::Internal)
-            },
+        match (
+            &log.topics.contains(&TOPIC_COLLECTION_CREATED),
+            &log.topics.contains(&TOPIC_TOKEN_MINTED),
+        ) {
+            (true, false) => ethabi::decode(
+                &[ParamType::Address, ParamType::String, ParamType::String],
+                &log.data.0,
+            )
+            .map(|params| Event::CollectionCreated {
+                collection: params[0].to_string(),
+                name: params[1].to_string(),
+                symbol: params[2].to_string(),
+            })
+            .map_err(|_| Web3Error::Internal),
+            (false, true) => ethabi::decode(
+                &[
+                    ParamType::Address,
+                    ParamType::Address,
+                    ParamType::Uint(256),
+                    ParamType::String,
+                ],
+                &log.data.0,
+            )
+            .map(|params| Event::TokenMinted {
+                collection: params[0].to_string(),
+                recipient: params[1].to_string(),
+                token_id: params[2].to_string(),
+                token_uri: params[3].to_string(),
+            })
+            .map_err(|_| Web3Error::Internal),
             (_, _) => unreachable!(),
         }
     }
@@ -77,20 +83,22 @@ struct AppState {
 
 type SharedState = Arc<RwLock<AppState>>;
 
-
 async fn event_listener(state: SharedState) -> Result<(), web3::Error> {
     let ws = web3::transports::WebSocket::new("ws://localhost:8545").await?;
     let web3 = web3::Web3::new(ws.clone());
-    let mut sub = web3.eth_subscribe().subscribe_logs(
-        FilterBuilder::default()
-        .topics(
-            Some(vec![TOPIC_COLLECTION_CREATED, TOPIC_TOKEN_MINTED]),
-            None,
-            None,
-            None,
+    let mut sub = web3
+        .eth_subscribe()
+        .subscribe_logs(
+            FilterBuilder::default()
+                .topics(
+                    Some(vec![TOPIC_COLLECTION_CREATED, TOPIC_TOKEN_MINTED]),
+                    None,
+                    None,
+                    None,
+                )
+                .build(),
         )
-        .build()
-    ).await?;
+        .await?;
 
     println!("Got subscription id: {:?}", sub.id());
 
@@ -147,7 +155,10 @@ async fn main() -> web3::Result {
 
     let shared_state = SharedState::default();
 
-    let _ = futures::join!(event_listener(shared_state.clone()), http_server(shared_state));
+    let _ = futures::join!(
+        event_listener(shared_state.clone()),
+        http_server(shared_state)
+    );
 
     Ok(())
 }
