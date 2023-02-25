931cd9718026a55fadc13de11c875d3bab3408a9
Andrei Orlov <aaorlov1@gmail.com>
2023-02-26 03:38:28 +0400

    Add frontend

---
+++ frontend/src/abi/SimpleNFT.tsx
@@ -0,0 +1,8 @@
+export const SimpleNFT = [
+    // Functions
+    "function createNewCollection(address,string,string)",
+    "function mint(address,address,uint256,string)",
+    // Events
+    "event CollectionCreated(address,string,string)",
+    "event TokenMinted(address,address,uint256,string)"
+];
